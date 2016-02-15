fd365692b417276c112a3fe634b47b5d67014e76
Jessica Sumner <jess.r.sumner@gmail.com>
2016-02-14 22:06:36 -0500

    Flattened the json hash so that it is a simple hash.

--- templater.rb
+++ templater.rb
@@ -12,7 +12,7 @@ class Templater
       variables_hash
     end
     puts open_html_file.gsub(/[<][*](\s?)(.+)(\s?)[*][>]/, variables_hash)
-    puts convert_json
+    puts flatten_json_hash(convert_json)
   end
 
   private
@@ -23,6 +23,18 @@ class Templater
 
   def convert_json
     JSON.parse(File.read(data_file))
+  end
+
+  def flatten_json_hash(hash)
+    hash.each_with_object({}) do |(key, value), new_hash|
+      if value.is_a?(Hash)
+        flatten_json_hash(value).map do |h_k, h_v|
+          new_hash["#{key}.#{h_k}".to_sym] = h_v
+        end
+      else
+        new_hash[key] = value
+      end
+    end
   end
 end
 
